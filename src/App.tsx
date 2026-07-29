a773b3c34d8ea9226cb7ee035d4b9cede4d674f7
eliascapasso <elias_capasso@live.com>
2026-07-29 14:53:00 -0300

    etiquetas de entorno

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ import NotFound from "./pages/NotFound";
 
 // Layout
 import AdminLayout from "./components/layouts/AdminLayout";
+import EnvironmentBadge from "./components/EnvironmentBadge";
 
 const queryClient = new QueryClient();
 
@@ -48,6 +49,7 @@ const App = () => {
       <TooltipProvider>
         <Toaster />
         <Sonner />
+        <EnvironmentBadge />
         <BrowserRouter>
           <Routes>
             <Route 
